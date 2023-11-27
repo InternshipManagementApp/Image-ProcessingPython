02e3644c29aeae3d0e007372d62fe807098677ac
AnitaGabor <79316759+AnitaGabor@users.noreply.github.com>
2023-11-27 19:26:49 +0200

    Update config and requriements file
    Up to date the config.ini and requerimetns.txt

--- imagetotext_tesseract.py
+++ imagetotext_tesseract.py
@@ -2,17 +2,16 @@
 
 import cv2, numpy 
 import pytesseract
-import os
 from configparser import ConfigParser
 import logging.config
 from pathlib import Path
 
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Anita\AppData\Local\Programs\Tesseract-OCR\tesseract.exe' 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Anita\AppData\Local\Programs\Tesseract-OCR\tesseract.exe' 
 
-def convertColorToGrayScale(image): #color picture to grayscale
+def convertColorToGrayScale(image): 
   return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
   
-def convertGrayScaleToBinary(image): #grayscale picture to binary
+def convertGrayScaleToBinary(image): 
   return cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
   
 def invertBlackAndWhite(image): #background is white and the foreground is black
@@ -22,45 +21,37 @@ def invertBlackAndWhite(image): #background is white and the foreground is black
     image = 255 - image
   return image
 
-def getNumberFromImage(image): #tesseract
+def getNumberFromImage(image, config): #tesseract
   #configuration: it will read only the specified language and for what to look for
-  return pytesseract.image_to_string(image, lang='eng', config='--psm 13 --oem 3 -c tessedit_char_whitelist=0123456789') 
+  return pytesseract.image_to_string(image, lang=config['TESS_LANGUAGE'], 
+    config='--psm ' + config['TESS_PAGESEGMEN'] + ' --oem ' + config['TESS_OCRENGINEMODE'] +' -c tessedit_char_whitelist=' + config['TESS_WHITELIST']) 
 
 def main():
   #Read config.ini file
   config_object = ConfigParser()
   config_object.read(Path("config.ini"))
   config = config_object["DEFAULT"]
-  print(config['THIS_EXAMPLE'])
-
-  #Get the password
-  account = config_object["ACCOUNT"]
-  print("Status is: ", account["status"])
-  #logtest
 
   logging.config.fileConfig('log_config.ini')
   logging.info('Starting .. ')
-  # logging.basicConfig(filename="log.txt", level=logging.DEBUG,format="%(asctime)s %(message)s")
-  # todo: move to config
   logging.info('Stop')
 
-  folderName = "ImagesClock"
-
-  # TODO: from pathlib import Path -- check this out
-  if os.path.exists("result.txt"):
-    os.remove("result.txt")
-  file = open("result.txt","a")
-  for imageName in os.listdir(folderName):
-    image = cv2.imread(os.path.join(folderName,imageName))
+  filePath = Path(config['OUTPUT_FILE'])
+  if filePath.exists():
+    filePath.unlink()
+  file = open(config['OUTPUT_FILE'],"a")
+  images = Path(config['FOLDER_NAME']).glob("*.jpg")
+  for imagePath in images:
+    image = cv2.imread(str(imagePath))
     
     #pre process tesseract
     image = convertColorToGrayScale(image)
     _, binaryImage = convertGrayScaleToBinary(image)
     binaryImage = invertBlackAndWhite(binaryImage)
-    cv2.imshow(imageName, binaryImage)
+    cv2.imshow(str(imagePath), binaryImage)
     cv2.waitKey(0)
-    text = getNumberFromImage(image)
-    file.write(imageName + " " +text)
+    text = getNumberFromImage(image, config)
+    file.write(str(imagePath) + " " +text)
     #print(text)
 
   logging.debug("Logging test...")  
