b52ed80d354405e57bb23d6b52ed1606e260d9b1
AnitaGabor <79316759+AnitaGabor@users.noreply.github.com>
2023-12-11 19:50:08 +0200

    Image smoothing and more structured code
    class definition, separate the utils function and try some image processing for the specific images

--- imagetotext_tesseract.py
+++ imagetotext_tesseract.py
@@ -4,28 +4,34 @@ import cv2, numpy
 import pytesseract
 from configparser import ConfigParser
 import logging.config
-from pathlib import Path
+from pathlib import Path 
+import utils
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Anita\AppData\Local\Programs\Tesseract-OCR\tesseract.exe' 
 
-def convertColorToGrayScale(image): 
-  return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  
-def convertGrayScaleToBinary(image): 
-  return cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
-  
-def invertBlackAndWhite(image): #background is white and the foreground is black
-  countWhite = numpy.sum(image > 0) #number of all pixel that are not black (255)
-  countBlack = numpy.sum(image == 0) #number of all pixel that are black (0)
-  if countBlack > countWhite:
-    image = 255 - image
-  return image
+class ImageToText:
+  def __init__ (self, image):
+    self.image = image
+    
+  def preprocess(self):
+    grayImage = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+    grayImage = cv2.medianBlur(grayImage,5)
+    _, binaryImage = cv2.threshold(grayImage, 0, 255, cv2.THRESH_OTSU)
+    binaryImage = utils.invertBlackAndWhite(binaryImage)
+    erodeImg = utils.erode(binaryImage)
+    dilateImg = utils.dilate(erodeImg)
+    openingImg = utils.opening(dilateImg)
+    return openingImg
 
-def getNumberFromImage(image, config): #tesseract
-  #configuration: it will read only the specified language and for what to look for
-  return pytesseract.image_to_string(image, lang=config['TESS_LANGUAGE'], 
-    config='--psm ' + config['TESS_PAGESEGMEN'] + ' --oem ' + config['TESS_OCRENGINEMODE'] +' -c tessedit_char_whitelist=' + config['TESS_WHITELIST']) 
-
+  def getTheImage(self):
+    return self.image
+    
+  def getNumberFromImage(self, config): #tesseract
+    #configuration: it will read only the specified language and for what to look for
+    return pytesseract.image_to_string(self.image, lang=config['TESS_LANGUAGE'], 
+      config='--psm ' + config['TESS_PAGESEGMEN'] + ' --oem ' + config['TESS_OCRENGINEMODE'] 
+      +' -c tessedit_char_whitelist=' + config['TESS_WHITELIST']) 
+      
 def main():
   #Read config.ini file
   config_object = ConfigParser()
@@ -39,21 +45,20 @@ def main():
   filePath = Path(config['OUTPUT_FILE'])
   if filePath.exists():
     filePath.unlink()
-  file = open(config['OUTPUT_FILE'],"a")
-  images = Path(config['FOLDER_NAME']).glob("*.jpg")
-  for imagePath in images:
-    image = cv2.imread(str(imagePath))
     
+  outputFile = open(config['OUTPUT_FILE'],"a")
+  imagesPath = Path(config['FOLDER_NAME']).glob("*.jpg")
+  
+  for imagePath in imagesPath:
     #pre process tesseract
-    image = convertColorToGrayScale(image)
-    _, binaryImage = convertGrayScaleToBinary(image)
-    binaryImage = invertBlackAndWhite(binaryImage)
+    objOfImage = ImageToText(cv2.imread(str(imagePath)))
+    binaryImage = objOfImage.preprocess()
+    
     cv2.imshow(str(imagePath), binaryImage)
     cv2.waitKey(0)
-    text = getNumberFromImage(image, config)
-    file.write(str(imagePath) + " " +text)
-    #print(text)
-
+    
+    text = objOfImage.getNumberFromImage(config)
+    outputFile.write(str(imagePath) + " " +text)
   logging.debug("Logging test...")  
 
 
